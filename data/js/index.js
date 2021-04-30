9823b29002921a9921b67529aa444a98f310e8bd
Anrijs <anrijs@anrijs.lv>
2021-04-30 18:29:57 +0300

    index: updated refresh script

--- data/js/index.js
+++ data/js/index.js
@@ -1,1 +1,61 @@
-function updateCards(){fetch("/data").then(e=>e.text()).then(e=>{let t=e.split("\n");var a=300;for(let e of t){let t=e.split(";");if(t.length<11)continue;let o=t[0],m=document.querySelector('[data-id*="'+o+'"]');if(!m)continue;let r=t[3],c=t[7];var n="co2-none";0==r?kalss="co2-none":n=r<1e3?"co2-ok":r<1400?"co2-warn":"co2-alert";var s="";s=c>90?"100":c>80?"90":c>70?"80":c>60?"70":c>50?"60":c>40?"50":c>30?"40":c>20?"30":c>10?"20":"10";var l="bluetooth";t[10]>parseInt(t[8])+5&&(l="bluetoothred",n="co2-none"),m.getElementsByClassName("cardimg")[0].src="/img/"+l+".png",m.getElementsByClassName("batt-val")[0].src="/img/battery_"+s+".png",m.getElementsByClassName("co2-txt")[0].textContent=r,m.getElementsByClassName("temp-val")[0].textContent=t[4],m.getElementsByClassName("humi-val")[0].textContent=t[6],m.getElementsByClassName("pres-val")[0].textContent=t[5],m.className="card "+n;let i=t[8]-t[9];i<a&&(a=i)}setTimeout(updateCards,1e3*a)})}setTimeout(updateCards,5e3);
+function updateCards() {
+  fetch("/data")
+  .then(response => response.text())
+  .then((dataStr) => {
+    let lines = dataStr.split("\n");
+    var nextUpdate = 300;
+    for (let ln of lines) {
+      let pt = ln.split(";");
+      if (pt.length < 11) {
+        continue;
+      }
+
+      let uid = pt[0];
+      let card = document.querySelector(`[data-id*="` + uid + `"]`);
+
+      if (!card) {
+        continue;
+      }
+
+      let co2 = pt[3];
+      let batt = pt[7];
+
+      var klass = "co2-none";
+      if (co2 == 0) kalss="co2-none";
+      else if (co2 < 1000) klass="co2-ok";
+      else if (co2 < 1400) klass="co2-warn";
+      else klass = "co2-alert";
+
+      var batimg = "";
+
+      if (batt > 90) batimg ="100";
+      else if (batt > 80) batimg ="90";
+      else if (batt > 70) batimg ="80";
+      else if (batt > 60) batimg ="70";
+      else if (batt > 50) batimg ="60";
+      else if (batt > 40) batimg ="50";
+      else if (batt > 30) batimg ="40";
+      else if (batt > 20) batimg ="30";
+      else if (batt > 10) batimg ="20";
+      else batimg = "10";
+      var btimg = "bluetooth";
+      if (pt[10] > (parseInt(pt[8])+5)) {
+        btimg = "bluetoothred";
+        klass = "co2-none";
+      }
+      card.getElementsByClassName("cardimg")[0].src = "/img/" + btimg + ".png";
+      card.getElementsByClassName("batt-val")[0].src = "/img/battery_" + batimg + ".png";
+      card.getElementsByClassName("co2-txt")[0].textContent = co2;
+      card.getElementsByClassName("temp-val")[0].textContent = pt[4];
+      card.getElementsByClassName("humi-val")[0].textContent = pt[6];
+      card.getElementsByClassName("pres-val")[0].textContent = pt[5];
+      card.className="card " + klass;
+
+      let u = pt[8] - pt[9];
+      if (u<nextUpdate)nextUpdate=u;
+    }
+    setTimeout(updateCards, (nextUpdate+10)*1000);
+  });
+}
+
+setTimeout(updateCards, 5000);
